4d835b098c01890fd7292afa90c65afba6330448
George Sotiropoulos <gsotiro@hotmail.com>
2025-11-30 18:29:03 +0200

    added gitignore

--- InsurancePOC.Api/obj/Debug/net9.0/InsurancePOC.Api.AssemblyInfo.cs
+++ InsurancePOC.Api/obj/Debug/net9.0/InsurancePOC.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InsurancePOC.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03bed16eb74eeb0c7949bd057de277d7cdf3c6a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("InsurancePOC.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InsurancePOC.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
